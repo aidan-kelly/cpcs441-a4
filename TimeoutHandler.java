205f5b4f3771847c8408d6056cc6fe96c44f9898
Aidan Kelly <it.is.aidan@gmail.com>
2017-11-30 12:12:37 -0700

    Forgot to add this bad boy

---
+++ TimeoutHandler.java
@@ -0,0 +1,17 @@
+package assignment4;
+import java.util.TimerTask;
+
+public class TimeoutHandler extends TimerTask {
+		Router router;
+		
+		//constructor definition
+		public TimeoutHandler(Router router){
+			this.router = router;
+		}
+
+		public void run() {
+			this.router.processTimeout();
+			
+		}
+
+}
